c74ba2bceadc11029b25be2bc127fa9bdafa57b4
freddie <dee.talipoff@gmail.com>
2021-12-04 23:59:27 +0500

    added organization controllers

---
+++ src/WebUI/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebUI.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator mediator;
+        private ILogger<BaseController> logger;
+
+        protected IMediator Mediator
+        {
+            get
+            {
+                if (mediator == null)
+                    mediator = HttpContext.RequestServices.GetService<IMediator>();
+                return mediator;
+            }
+        }
+    }
+}
---
+++ src/WebUI/Controllers/OrganizationController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Models;
+using Application.Organizations.Queries.OrganizationDetail;
+using Application.Organizations.Queries.OrganizationsList;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    [Route("api/organizations")]
+    [ApiController]
+    public class OrganizationController : BaseController
+    {
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Result), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<OrganizationDetailDto>> Get([FromRoute] int id)
+        {
+            return Ok(await Mediator.Send(new OrganizationDetailQuery
+            {
+                Id = id
+            }));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<BaseView<OrganizationsListDto>>> GetAll([FromQuery] OrganizationsListQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+    }
+}
