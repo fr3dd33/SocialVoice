0b25026451ee46e88f895eef1e0ed43ce57e5776
freddie <dee.talipoff@gmail.com>
2021-12-05 12:18:55 +0500

    issue voting limited per user

--- src/Application/Issues/Commands/VoteProIssue/VoteProIssueCommandHandler.cs
+++ src/Application/Issues/Commands/VoteProIssue/VoteProIssueCommandHandler.cs
@@ -1,5 +1,10 @@
-﻿using Application.Common.Interfaces;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entites;
 using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,17 +13,42 @@ namespace Application.Issues.Commands.VoteProIssue
     public class VoteProIssueCommandHandler : IRequestHandler<VoteProIssueCommand>
     {
         private readonly ISocialVoiceDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public VoteProIssueCommandHandler(ISocialVoiceDbContext context)
+        public VoteProIssueCommandHandler(
+            ISocialVoiceDbContext context,
+            IHttpContextAccessor httpContextAccessor
+        )
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<Unit> Handle(VoteProIssueCommand request, CancellationToken cancellationToken)
         {
-            var issue = await _context.Issues.FindAsync(request.Id);
+            var issue = await _context.Issues.Include(x => x.Voters).FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (issue == null)
+            {
+                throw new BadRequestException("Issue not found");
+            }
+
+            _httpContextAccessor.HttpContext.Request.Headers.TryGetValue("User-Agent", out var userAgent);
+            if (issue.Voters.Any(x =>
+                x.IPAddress == _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString() &&
+                x.UserAgent == userAgent
+            ))
+            {
+                throw new BadRequestException("You can not vote more than once");
+            }
 
             issue.Pros += 1;
+
+            issue.Voters.Add(new Voter
+            {
+                UserAgent = userAgent,
+                IPAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString()
+            });
 
             await _context.SaveChangesAsync(cancellationToken);
 
